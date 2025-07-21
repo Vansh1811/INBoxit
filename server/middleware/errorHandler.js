ee7d9037b912a48e54c0852b0a7791ed94e9cd6d
Vansh Sharma <s.vansh1811@gmail.com>
2025-07-21 23:51:00 +0530

    Update errorHandler.js

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -17,7 +17,7 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === 'ValidationError') {
     return res.status(400).json({ 
       error: 'Validation Error', 
-      details: Object.values(err.errors).map(e => e.message)
+      details: Object.values(err.errors).map(e => e.message) 
     });
   }
 
