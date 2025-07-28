1b197093937ba323aa9be2b78765796e6d0f639a
Vansh Sharma <s.vansh1811@gmail.com>
2025-07-28 22:59:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,9 +7,7 @@ const gmailRoutes = require('./routes/gmail');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const servicesRoutes = require('./routes/services');
-// const { detectPlatforms, DetectedService } = require('./models/DetectedService'); 
-// const { apiLimiter } = require('./middleware/rateLimiter');
-// const { errorHandler, notFoundHandler } = require('./middleware/errorHandler');  
+ 
 const logger = require('./utils/logger'); 
 require('dotenv').config(); 
   
