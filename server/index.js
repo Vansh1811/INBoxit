00bbb2428c88f9432c61873b0a7b3950331901f1
Vansh Sharma <s.vansh1811@gmail.com>
2025-07-28 22:58:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const passport = require('./config/passport');
