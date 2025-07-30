d1e3b833fb06d7a601256b591d84bf2b43e48826
Vansh Sharma <s.vansh1811@gmail.com>
2025-07-30 22:47:39 +0530

    Update DetectedService.js

--- server/models/DetectedService.js
+++ server/models/DetectedService.js
@@ -6,7 +6,7 @@ const DetectedServiceSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'User',
     required: true,  
-  }, 
+  },  
   platform: String,
   sender: String,
   date: String,
